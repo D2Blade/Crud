652b81dd81cbfa7a551e8dad26d32d581733abe0
Demir001 <109363690+D2Blade@users.noreply.github.com>
2024-02-07 01:25:54 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from input_cliente import cadastrar_cliente
 
-cadastrar_cliente()
+
+print("[CRUD - BASICO]")
+print("1 - Criar\n2- Deletar\n3 - Editar")
+
+choice = input("Selecione uma Opção:")
+
+if choice == '1':
+
+    cadastrar_cliente()
+
+elif choice == '2':
+
+    print("Em construção")
+
+elif choice == '3':
+
+    print("Em construção")
+
+else:
+
+    print("Calma calabreso")
